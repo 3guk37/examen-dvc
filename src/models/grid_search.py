63a907f921bde9177ec009706684bc10c58254cb
Eddie <ebelsch@physnet.uni-hamburg.de>
2026-03-20 10:28:30 +0100

    change params in grid search

--- src/models/grid_search.py
+++ src/models/grid_search.py
@@ -7,9 +7,9 @@ X_train = pd.read_csv("data/processed_data/X_train_scaled.csv")
 y_train = pd.read_csv("data/processed_data/y_train.csv").squeeze()
 
 param_grid = {
-    "n_estimators": [50, 200],
-    "max_depth": [None, 5, 10],
-    "min_samples_split": [2, 5]
+    'n_estimators': [100, 200, 500],
+    'max_depth': [None, 10, 20, 30],
+    'min_samples_split': [2, 5, 10]
 }
 
 grid_search = GridSearchCV(RandomForestRegressor(random_state=42), param_grid, cv=3, scoring="r2", n_jobs=-1)
